cf683174a20b32b7830b7cffceeb7af3f0f82256
QuocPhan9 <ren737100@gmail.com>
2025-05-16 13:59:35 +0700

    Local changes to userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -48,6 +48,18 @@ const editUser = async (req, res, next) => {
 //===== Follow/Unfollow User =====//
 const followUnfollowUser = async (req, res, next) => {
     try {
+        const userToFollowId = req.params.id;
+        if (req.user.id === userToFollowId) {
+            return next(new HttpError("You can't follow/unfollow yourself", 422));
+        }
+
+        const currentUser = await UserModel.findById(req.user.id);
+        const isFollowing = currentUser?.following?.includes(userToFollowId);
+
+        //follow if not following, else unfollow is already folowing
+        if (!isFollowing) {
+            const updatedUser = await UserModel.findByIdAndUpdate(userToFollowId, {$push: {followers: req.user.id}}, {new: true});
+        }
         res.json("Follow/Unfollow User");
     } catch (error) {
         return next(new HttpError(error));
